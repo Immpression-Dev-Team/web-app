480b9788e0747b313d7774cbd898c4b55e3ac385
Mahmoud <tkhattab1999@gmail.com>
2024-11-30 15:37:02 -0500

    fix(api_url.js): add vercel url

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,7 +1,7 @@
-const ENV = import.meta.env.VITE_ENV;
+const ENV = import.meta.env.VITE_APP_ENV;
 const API_URL =
     ENV === "prod"
-        ? "https://immpression.art/api"
+        ? "https://immpression-backend.vercel.app"
         : "http://localhost:4000";
 
 export { API_URL };
