22d9f9bb787d2650250d7627ab6e43d843b361c6
Premo <ajaipremo@gmail.com>
2025-03-05 18:26:46 -0500

    feat(husky): added husky (uses docker)

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: '3000',
     host: true,
     strictPort: true,
     allowedHosts: ["frontend-web", "localhost"],
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  base: "/",
-});
+  base: '/',
+})
