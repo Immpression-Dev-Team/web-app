ec58bf45ab5e1705738b668e0230600b5426ec24
Premo <ajaipremo@gmail.com>
2025-03-05 18:39:46 -0500

    fix(husky): changed husky script instructions

--- husky-setup.sh
+++ husky-setup.sh
@@ -3,6 +3,10 @@
 # containerized version of husky.
 # TODO: RUN THIS SCRIPT IN EACH SERVICE. I HAVE PROVIDED THIS SCRIPT IN EACH
 
+# THESE COMMANDS DO NOT WORK IN WINDOWS POWERSHELL OR CMD...
+# THEY NEED TO BE RUN FROM GIT BASH TERMINAL IF ON WINDOWS.
+# IF ON LINUX OR MAC, THEY CAN BE RUN FROM TERMINAL.
+
 # To run this script, run this command on all OS'
 # (requires Git to be installed):
 # - sh husky-setup.sh
@@ -13,3 +17,6 @@ git config core.hooksPath .husky
 git config --get core.hooksPath
 
 # Output should be ".husky"
+
+# Additionally, the 'commit-msg' file in the .husky folder needs
+# to be executable in order to work.
